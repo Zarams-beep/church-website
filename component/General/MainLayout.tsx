706660af46315a339c941c30e59ff5596cec79fa
zaram-beeps <chukwumaobidivine87@gmail.com>
2026-01-04 22:06:57 +0100

    c

--- component/General/MainLayout.tsx
+++ component/General/MainLayout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import SplashScreen from "./Splash";
 import HeaderBar2 from "./header2";
 import Footer from "./Footer";
@@ -13,6 +14,7 @@ interface Props {
 export default function MainLayoutSection({ children }: Props) {
   const [loading, setLoading] = useState(true);
   const [is580, setIs580] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 1500);
@@ -30,18 +32,31 @@ export default function MainLayoutSection({ children }: Props) {
     return () => window.removeEventListener("resize", updateWindowWidth);
   }, []);
 
+  // Check if current page is a community article page
+  // This matches: /community/[category]/[slug]
+  const communityArticle = pathname?.match(/\/community\/[^/]+\/[^/]+$/) !== null;
+
+  // Debug - remove this after testing
+  console.log('Current pathname:', pathname);
+  console.log('Is community article:', communityArticle);
+
   return (
-     <>
+    <>
       {loading ? (
         <SplashScreen />
       ) : (
         <>
-          {is580 ? <HeaderBar3 /> : <HeaderBar2 />}
+          {/* Only show header if NOT on community article page */}
+          {!communityArticle && (is580 ? <HeaderBar3 /> : <HeaderBar2 />)}
+          
           {children}
-          <Footer />
-          <FloatingWhatsApp /> 
+          
+          {/* Only show footer if NOT on community article page */}
+          {!communityArticle && <Footer />}
+          
+          <FloatingWhatsApp />
         </>
       )}
-     </>
+    </>
   );
 }
